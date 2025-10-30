6592640cf95d72a83c0621d8d919daee401ea7b3
Joan Toro Ortiz <91379516+mrjoantoro@users.noreply.github.com>
2025-10-29 22:24:52 -0300

    Fix import router

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,21 +1,21 @@
 
-import { Route } from "express";
+import { Router } from "express";
 import { validate } from "../middlewares/validate.js";
 import { auth } from "../middlewares/auth.js";
 import { registerSchema, loginSchema, updateUserSchema } from "../schemas/userSchemas.js";
 import { register, login, me, updateMe, listUsers } from "../controllers/userController.js";
 
-const router = Route();
+const router = Router();
 
 // Públicos 
-router.post("auth/register", validate(registerSchema), register);
-router.post("auth/login", validate(loginSchema), login);
+router.post("/auth/register", validate(registerSchema), register);
+router.post("/auth/login", validate(loginSchema), login);
 
 // Autenticación
-router.get("users/me", auth, me);
-router.path("users/me", auth, validate(updateUserSchema), updateMe);
+router.get("/users/me", auth, me);
+router.patch("/users/me", auth, validate(updateUserSchema), updateMe);
 
 // (Opcional) Admin
-router.get("admin/users", auth, listUsers);
+router.get("/admin/users", auth, listUsers);
 
 export default router;
